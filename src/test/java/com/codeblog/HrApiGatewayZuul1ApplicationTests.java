8101b7922942038a1cde4e7debbf352b7ae39e16
macindex <machollanda@gmail.com>
2022-05-26 11:29:05 -0300

    config postgresql

---
+++ src/test/java/com/codeblog/HrApiGatewayZuul1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuul1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
