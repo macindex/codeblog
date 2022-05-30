8335e992578204e4215d5a18727d25e7dfd2c7bf
macindex <machollanda@gmail.com>
2022-05-30 14:47:00 -0300

    add controller

---
+++ src/java/com/codeblog/controller/CodeblogController.java
@@ -0,0 +1,31 @@
+package com.codeblog.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.codeblog.model.Post;
+import com.codeblog.service.CodeblogService;
+
+@Controller
+public class CodeblogController {
+	
+	@Autowired
+	CodeblogService codeblogService;
+	
+	@RequestMapping(value = "/posts", method = RequestMethod.GET)
+	public ModelAndView getPosts() {
+		//String viewName;
+		ModelAndView mv = new ModelAndView("posts");
+		List<Post> posts = codeblogService.findAll();
+		//String attributeName;
+		mv.addObject("posts", posts);
+		return mv;
+		
+	}
+
+}
