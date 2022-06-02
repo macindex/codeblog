a7298af8675497b51d17e10cf34ea0d00e3e4d71
macindex <machollanda@gmail.com>
2022-06-02 15:17:31 -0300

    rota id e arquivo postDetails

--- src/java/com/codeblog/controller/CodeblogController.java
+++ src/java/com/codeblog/controller/CodeblogController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -24,6 +25,16 @@ public class CodeblogController {
 		List<Post> posts = codeblogService.findAll();
 		//String attributeName;
 		mv.addObject("posts", posts);
+		return mv;
+		
+	}
+	@RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+	public ModelAndView getPostsDetails(@PathVariable("id") long id) {
+		//String viewName;
+		ModelAndView mv = new ModelAndView("PostDetails");
+		Post post = codeblogService.findById(id);
+		//String attributeName;
+		mv.addObject("post", post);
 		return mv;
 		
 	}
